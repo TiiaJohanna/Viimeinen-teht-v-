5350b0bfbd8712224c0a3a31fb48e63f0b79b0f0
TiiaJohanna <n3titi00@students.oamk.fi>
2024-03-09 14:54:24 +0200

    suurinpiirtein

---
+++ JavaScript/Functions.js
@@ -0,0 +1,56 @@
+// Määritetään nettisivut
+const url = 'https://api.openweathermap.org/data/2.5/weather?'
+const icon_url = 'https://openweathermap.org/img/wn/'
+const api_key = 'd35c404e900acb79ab8ec1e74f602cb3'
+
+
+// Muuttujat
+const temp_span = document.querySelector('span#temp')
+const speed_span = document.querySelector('span#speed')
+const direction_span = document.querySelector('span#direction')
+const description_span = document.querySelector('span#description')
+const icon_img = document.querySelector('img')
+
+
+// Määritetään sijainti
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude.toFixed(3)
+            const lng = position.coords.longitude.toFixed(3)
+            document.querySelector('#lat').innerHTML = lat + ', '
+            document.querySelector('#lng').innerHTML = lng
+            getWeather(position.coords.latitude,position.coords.longitude)
+        }),(error => {
+            alert(error)
+        })
+    } else {
+        alert("Your browser does not support geolocation!")
+    }
+}
+
+
+// Määritetään sää
+const getWeather = (lat, lng) => {
+    const address = url +
+    'lat=' + lat +
+    '&lon=' + lng +
+    '&units=metric' +
+    '&appid=' + api_key
+    axios.get(address)
+        .then(response => {
+            const json = response.data
+            temp_span.innerHTML = json.main.temp + '&#8451;'
+            speed_span.innerHTML = json.wind.speed + ' m/s'
+            direction_span.innerHTML = json.wind.deg + '&#176'
+            description_span.innerHTML = json.weather[0].description
+            const image = icon_url + json.weather[0].icon + '@2x.png'
+            icon_img.src = image
+        }).catch(error => {
+            alert(error)
+        })
+}
+
+
+// Suorittaa funktion getLocation
+getLocation()
